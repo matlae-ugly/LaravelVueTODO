db1209c69729d1627c8922e5b5ad65f4c6ec66e2
Igors Trimailovs <igors.trimailovs@bk.ru>
2020-04-04 14:52:28 +0300

    upd db

--- config/database.php
+++ config/database.php
@@ -1,7 +1,8 @@
+
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL = parse_url("postgres://ncbiiltnmougvw:45416c0d8075d61575c1cadabc2e1561a59ea6c8bc3514d41c4236870e08f57f@ec2-3-91-112-166.compute-1.amazonaws.com:5432/d35m1jjhbhssij");
 return [
 
     /*
@@ -14,8 +15,8 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-
-    'default' => env('DB_CONNECTION', 'mysql'),
+   
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +67,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            'database' => ltrim($DATABASE_URL['path'], '/'),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
